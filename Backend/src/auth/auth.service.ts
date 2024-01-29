063c62b30c6a9df835f54379db6f12f9ab07f698
Wisdom Eleeza <whiy07@gmail.com>
2024-01-29 16:01:55 +0000

    Working on prisma database connection

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async signIn(username: string, password: string): Promise<any> {}
+}
