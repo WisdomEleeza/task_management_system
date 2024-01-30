a3c987df0900b8ab60d9307f151e7e3046ee90c3
Wisdom Eleeza <whiy07@gmail.com>
2024-01-30 09:51:25 +0000

    Working on auth.services

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+  ) {}
 
-  async signIn(username: string, password: string): Promise<any> {
-    const salt = 10;
+  async signup(dto: AuthDto): Promise<any> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(dto.password, salt);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          username: dto.username,
+          email: dto.email,
+          password: hashedPassword,
+        },
+      });
+      return user;
+    } catch (error) {}
   }
 }
