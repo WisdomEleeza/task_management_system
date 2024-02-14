4f47e03cdb814b31745f169754f79519e91eb8f8
Wisdom Eleeza <whiy07@gmail.com>
2024-02-14 07:21:47 +0000

    Writing test case for signup

--- Backend/src/auth/auth.service.spec.ts
+++ Backend/src/auth/auth.service.spec.ts
@@ -3,21 +3,42 @@ import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { AuthDto } from './dto';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let PrismaServiceMock: jest.Mocked<PrismaService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService, PrismaService, JwtService, ConfigService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    PrismaServiceMock = module.get(PrismaService) as jest.Mocked<PrismaService>;
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 
-  // it('should create user', () => {});
+  describe('Sign Up', () => {
+    it('should signup a user', async () => {
+      // Arrange
+      const mockAuthDto: AuthDto = {
+        username: 'test-username',
+        email: 'test@example.com',
+        password: 'password123',
+      };
+
+      const mockUser = {
+        id: 1,
+        username: 'test-username',
+        email: 'test@example.com',
+        password: 'hashedPassword',
+      };
+
+      //
+    });
+  });
 });
